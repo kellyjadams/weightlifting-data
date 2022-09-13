a200a2158e8e485671b5e4a3dedf1275fb3240e4
Kelly Adams <81118926+kellyjadams@users.noreply.github.com>
2022-09-13 12:05:48 -0700

    Create create-schema.sql

---
+++ create-schema.sql
@@ -0,0 +1,38 @@
+CREATE SCHEMA weightlifting;
+SET search_path = weightlifting;
+
+ CREATE TABLE workout (
+   "workout_date" DATE,
+   "exercise_id" INTEGER,
+   "category_id" INTEGER,
+   "weight_lbs" INTEGER,
+   "reps" INTENGER
+ );
+ 
+CREATE TABLE exercises (
+  "exercise_id" INTEGER,
+  "exercise_name" VARCHAR(50),
+  "category_id" INTEGER
+); 
+
+INSERT INTO categories
+  ("exercise_id, "exercise_name", "category_id")
+VALUES
+  (1, 'Deadlift', 4),
+  (2, 'Bench Press', 1),
+  (3, 'Squat', 3),
+  (4, 'Overhead Press', 1),
+  (5, 'Row', 2);
+
+CREATE TABLE categories (
+  "category_id" INTEGER,
+  "category_name" VARCHAR(50)
+); 
+
+INSERT INTO categories
+  ("category_id", "category_name")
+VALUES
+  (1, 'Push'),
+  (2, 'Pull'),
+  (3, 'Legs'),
+  (4, 'Hinge');
